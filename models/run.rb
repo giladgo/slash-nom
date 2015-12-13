d77712e85e0f31199e76bcaa6d844c627045b2b4
Gilad Goldberg <ggoldberg@paypal.com>
2015-12-13 16:18:56 +0200

    Removing slash-coffee stuff

--- models/run.rb
+++
@@ -1,7 +0,0 @@
-class Run < ActiveRecord::Base
-  has_many :orders
-
-  scope :active, -> { where(active: true) }
-  scope :in_team, ->(team) { where(team_id: team) }
-  scope :by_user, ->(user) { where(user_id: user) }
-end
