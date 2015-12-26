bc80c9584c4f93bfe6cd02e46158c8dd03cab66c
Gilad Goldberg <ggoldberg@paypal.com>
2015-12-26 16:26:18 +0200

    Add empty message for list command

--- app.rb
+++ app.rb
@@ -43,7 +43,11 @@ def list(rest, params)
     users = decls.map(&:user_name)
     "#{rest.display_name}: #{users.join(', ')}"
   end
-  respond "*For #{DateTime.now.strftime("%A, %B %-d, %Y")}, people want to go to:*\n" + lines.join("\n")
+  if lines.empty?
+    respond "*Nobody wants to go anywhere today (#{DateTime.now.strftime("%A, %B %-d, %Y")}), be the first to show an interest in a place by entering `/um go [place-name]`!*"
+  else
+    respond "*For #{DateTime.now.strftime("%A, %B %-d, %Y")}, people want to go to:*\n" + lines.join("\n")
+  end
 end
 
 def help
