2fc68953e32239e91b7fc3b9fb219dcc6826371e
Gilad Goldberg <ggoldberg@paypal.com>
2016-01-03 17:58:08 +0200

    Revert "Support for creating a restaurant with emoji"
    This reverts commit f1ee634952c53001489f478c40a755a5cdbb031d.

--- models/restaurant.rb
+++ models/restaurant.rb
@@ -1,20 +1,10 @@
-require './lib/utils'
-
 class Restaurant < ActiveRecord::Base
   has_many :declarations
 
   scope :in_team, ->(team) { where(team_id: team) }
 
   def self.by_input(input)
-    Utils.emoji?(input) ? self.by_emoji(input) : self.by_name(input)
-  end
-
-  def self.by_name(input)
-    self.where('lower(name) = ?', input.downcase).first_or_create(name: input.downcase)
-  end
-
-  def self.by_emoji(emoji)
-    self.first_or_create(emoji: emoji, name: Utils.unemojify(emoji))
+    self.where('lower(name) = ? OR emoji = ?', input.downcase, input.downcase).first_or_create(name: input)
   end
 
   def init_declaration(user_id, user_name, channel_id)
@@ -26,8 +16,7 @@ class Restaurant < ActiveRecord::Base
   end
 
   def display_name
-    title_name = self.name.present? ? self.name.titleize : ''
-    self.emoji.present? ? "#{self.emoji} #{title_name}" : title_name
+    self.emoji.present? ? "#{self.emoji} #{self.name.titleize}" : self.name.titleize
   end
 
 end
