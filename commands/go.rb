3de353932a3230af9cb149605b9642d7edf65c94
Gilad Goldberg <ggoldberg@paypal.com>
2016-03-13 16:48:17 +0200

    Fix @here notification

--- commands/go.rb
+++ commands/go.rb
@@ -9,7 +9,7 @@ class SlashUmServer
       decl.save!
       if set_pinned_message(params['team_id'], params['channel_id'])
         # new message
-        respond_in_channel "@here, #{params['user_name']} wants to go to #{decl.restaurant.display_name}! Join them by typing `/um go <place>.`"
+        @slack_client.chat_postMessage(channel: params['channel_id'], text: "@here, #{params['user_name']} wants to go to #{decl.restaurant.display_name}! Join them by typing `/um go <place>.`", as_user: true)
       else
         respond "You want to go to #{decl.restaurant.display_name}!"
       end
