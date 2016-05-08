41ba7bfada3e491ace7fceab83a7668a5c136f50
Gilad Goldberg <ggoldberg@paypal.com>
2016-05-08 17:17:14 +0300

    Debug testing

--- commands/go.rb
+++ commands/go.rb
@@ -1,6 +1,7 @@
 class SlashNomServer
 
   def go(rest, params)
+    puts '############################', params['slack_bot_token']
     if not in_channel?(params['channel_id'], params['slack_bot_token'])
       respond "Please call me from a channel I've been invited to."
     else
